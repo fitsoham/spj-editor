411eae245ff01ec0f19b5e8da0fd7188150c2733
saurabh sachan <saurabh.sachan@spacejoy.com>
2021-04-22 17:43:08 +0530

    WIP

--- components/Home/Hero/v1.tsx
+++ components/Home/Hero/v1.tsx
@@ -12,3 +12,23 @@ const v1 = () => {
 };
 
 export default v1;
+
+{
+  /* <Cell width={1}>
+        <HeroTextWrapper>
+          <h1>Live in joy</h1>
+          <h3>Online interior design services by Spacejoy</h3>
+          <p>
+            Get your room designed by professional interior designer and shop high-quality furniture and decor from your
+            favorite brands.
+          </p>
+        </HeroTextWrapper>
+      </Cell> */
+}
+{
+  /* <Image src="/images/b1.jpg" alt="spacejoy Logo" height={500} width={350} />
+      <Image src="/images/b1.jpg" alt="spacejoy Logo" height={500} width={350} />
+      <Image src="/images/b2.jpg" alt="spacejoy Logo" height={500} width={350} />
+      <Image src="/images/b3.jpg" alt="spacejoy Logo" height={500} width={350} />
+      <Image src="/images/b4.jpg" alt="spacejoy Logo" height={500} width={350} /> */
+}
