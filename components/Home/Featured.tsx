a1ab923dcf5e00ecf4b84ad062e9cd60b0394137
saurabh sachan <saurabh.sachan@spacejoy.com>
2021-05-28 11:31:46 +0530

    update

--- components/Home/Featured.tsx
+++ components/Home/Featured.tsx
@@ -11,6 +11,13 @@ const Featured = () => {
         description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis reprehenderit reiciendis officia vero ab nostrum
       asperiores aperiam? Illo vitae recusandae, incidunt quas sapiente quo maxime impedit cum, hic odio temporibus!"
       />
+      <div className="bg-gray-100 h-64 flex items-center justify-center">
+        <div className="shadow-sm bg-white h-32 w-32 rounded-xl flex items-center justify-center mx-5">s</div>
+        <div className="shadow-sm bg-white h-32 w-32 rounded-xl flex items-center justify-center mx-5">s</div>
+        <div className="shadow-sm bg-white h-32 w-32 rounded-xl flex items-center justify-center mx-5">s</div>
+        <div className="shadow-sm bg-white h-32 w-32 rounded-xl flex items-center justify-center mx-5">s</div>
+        <div className="shadow-sm bg-white h-32 w-32 rounded-xl flex items-center justify-center mx-5">s</div>
+      </div>
     </div>
   );
 };
--- components/common/SectionTitle.tsx
+++ components/common/SectionTitle.tsx
@@ -3,10 +3,18 @@ import React from 'react';
 const SectionTitle = ({ accent, feature, title, description }) => {
   return (
     <div className="bg-gray-0">
-      <div className="container text-center mx-auto py-10">
-        <p className={`text-indigo-500 text-sm mb-2`}>{feature}</p>
+      <div className="container text-center mx-auto py-20">
+        <p
+          className={`
+          ${accent === 'indigo' && 'text-indigo-500'}
+          ${accent === 'pink' && 'text-pink-500'}
+          ${accent === 'yellow' && 'text-yellow-500'} 
+          text-sm mb-2`}
+        >
+          {feature}
+        </p>
         <h2 className="text-gray-900 text-3xl mb-5">{title}</h2>
-        <p className="text-gray-500 max-w-5xl mx-auto">{description}</p>
+        <p className="text-gray-500 max-w-4xl mx-auto">{description}</p>
       </div>
     </div>
   );
