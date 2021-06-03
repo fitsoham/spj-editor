040def816e44a9b970bc3b47ddc7bce94709fb9a
saurabh sachan <saurabh.sachan@spacejoy.com>
2021-06-03 23:43:58 +0530

    update

--- components/Help/Ecommerce.tsx
+++ components/Help/Ecommerce.tsx
@@ -1,4 +1,6 @@
+import HelpData from '@mocks/HelpData';
 import React from 'react';
+import AccordionItem from './AccordionItem';
 
 const Ecommerce: React.FC = () => {
   return (
@@ -8,126 +10,9 @@ const Ecommerce: React.FC = () => {
           <div className="max-w-7xl mx-auto px-4 sm:py-16 sm:px-6 lg:px-8">
             <div className="max-w-3xl mx-auto divide-y-2 divide-gray-200">
               <dl className="mt-6 space-y-6 divide-y divide-gray-300">
-                <div className="pt-0">
-                  <dt className="text-lg">
-                    <button
-                      type="button"
-                      className="text-left w-full flex justify-between items-start text-gray-400"
-                      aria-controls="faq-0"
-                      aria-expanded="false"
-                    >
-                      <span className="font-medium text-gray-900">What&#039;s the best thing about Switzerland?</span>
-                      <span className="ml-6 h-7 flex items-center">
-                        <svg
-                          className="rotate-0 h-6 w-6 transform"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          aria-hidden="true"
-                        >
-                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
-                        </svg>
-                      </span>
-                    </button>
-                  </dt>
-                  <dd className="mt-2 pr-12" id="faq-0">
-                    <p className="text-base text-gray-500">
-                      I don&#039;t know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing
-                      elit. Quas cupiditate laboriosam fugiat.
-                    </p>
-                  </dd>
-                </div>
-                <div className="pt-6">
-                  <dt className="text-lg">
-                    <button
-                      type="button"
-                      className="text-left w-full flex justify-between items-start text-gray-400"
-                      aria-controls="faq-0"
-                      aria-expanded="false"
-                    >
-                      <span className="font-medium text-gray-900">What&#039;s the best thing about Switzerland?</span>
-                      <span className="ml-6 h-7 flex items-center">
-                        <svg
-                          className="rotate-0 h-6 w-6 transform"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          aria-hidden="true"
-                        >
-                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
-                        </svg>
-                      </span>
-                    </button>
-                  </dt>
-                  <dd className="mt-2 pr-12" id="faq-0">
-                    <p className="text-base text-gray-500">
-                      I don&#039;t know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing
-                      elit. Quas cupiditate laboriosam fugiat.
-                    </p>
-                  </dd>
-                </div>
-                <div className="pt-6">
-                  <dt className="text-lg">
-                    <button
-                      type="button"
-                      className="text-left w-full flex justify-between items-start text-gray-400"
-                      aria-controls="faq-0"
-                      aria-expanded="false"
-                    >
-                      <span className="font-medium text-gray-900">What&#039;s the best thing about Switzerland?</span>
-                      <span className="ml-6 h-7 flex items-center">
-                        <svg
-                          className="rotate-0 h-6 w-6 transform"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          aria-hidden="true"
-                        >
-                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
-                        </svg>
-                      </span>
-                    </button>
-                  </dt>
-                  <dd className="mt-2 pr-12" id="faq-0">
-                    <p className="text-base text-gray-500">
-                      I don&#039;t know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing
-                      elit. Quas cupiditate laboriosam fugiat.
-                    </p>
-                  </dd>
-                </div>
-                <div className="pt-6">
-                  <dt className="text-lg">
-                    <button
-                      type="button"
-                      className="text-left w-full flex justify-between items-start text-gray-400"
-                      aria-controls="faq-0"
-                      aria-expanded="false"
-                    >
-                      <span className="font-medium text-gray-900">What&#039;s the best thing about Switzerland?</span>
-                      <span className="ml-6 h-7 flex items-center">
-                        <svg
-                          className="rotate-0 h-6 w-6 transform"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          aria-hidden="true"
-                        >
-                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
-                        </svg>
-                      </span>
-                    </button>
-                  </dt>
-                  <dd className="mt-2 pr-12" id="faq-0">
-                    <p className="text-base text-gray-500">
-                      I don&#039;t know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing
-                      elit. Quas cupiditate laboriosam fugiat.
-                    </p>
-                  </dd>
-                </div>
+                {HelpData?.map((item, index) => (
+                  <AccordionItem key={item.question} data={item} index={index} />
+                ))}
               </dl>
             </div>
           </div>
--- components/Search/SearchBox.tsx
+++ components/Search/SearchBox.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
 
@@ -36,13 +37,27 @@ const SearchBox = () => {
                 id="first_name"
                 autoComplete="none"
                 placeholder="search"
-                className="py-5 pl-5 pr-20 outline-none block w-full shadow-sm focus:shadow-lg focus:ring-transparent border border-gray-100 focus:border-gray-100 rounded-lg"
+                className="py-5 pl-5 pr-20 outline-none block w-full shadow-sm focus:shadow-lg focus:ring-transparent border border-gray-100 focus:border-gray-100 rounded-xl"
               />
-              <div className="absolute right-0 top-0 bottom-0 w-16 bg-gray-50 flex justify-center text-center items-center border border-gray-100 rounded-lg">
+              <div className="absolute right-0 top-0 bottom-0 w-16 bg-gray-50 flex justify-center text-center items-center border border-gray-100 rounded-xl">
                 <span className="text-xs text-yellow-500">clear</span>
               </div>
             </div>
-            <div className="w-full p-5 bg-white border border-gray-100 mt-2 shadow-sm rounded-lg">hi</div>
+            <div className="w-full p-5 bg-white border border-gray-100 mt-2 shadow-sm rounded-xl">
+              <div className="flex items-center space-x-2 lg:space-x-4">
+                <Image
+                  src="https://res.cloudinary.com/spacejoy/image/upload/fl_lossy,q_auto/v1610707589/server/designs/render/60017284039102001c1e3dc9.jpg"
+                  className="rounded-md"
+                  alt=""
+                  height={'60'}
+                  width={'60'}
+                />
+                <div className="leading-6 space-y-1">
+                  <h4>Leslie Alexander</h4>
+                  <p className="text-gray-500 text-sm">Boho in designs</p>
+                </div>
+              </div>
+            </div>
           </form>
         </AnimateBox>
       </div>
