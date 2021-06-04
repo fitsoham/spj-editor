185fd49abfaeb758c6a03359ae8ca67a3cff9573
saurabh sachan <saurabhdsachan@gmail.com>
2021-06-04 15:49:38 +0530

    Feature/seo config (#4)
    * added api wrapper, seo and pricing, ts
    
    * updated per page robots props
    
    * resolved error
    
    * updated links
    
    * fixed build issue ts
    
    Co-authored-by: Soham Bhaumik <soham.bhaumik@spacejoy.com>

---
+++ utils/SEO/index.tsx
@@ -0,0 +1,9 @@
+import CommonSEO from './default.config';
+import HomePageSEO from './home.config';
+import PricingPageSEO from './pricing.config'
+
+export {
+    CommonSEO,
+    HomePageSEO,
+    PricingPageSEO
+}
