a7e48ee0b8f2c1d9d687e43fd2ef362258e554a9
saurabh sachan <saurabh.sachan@spacejoy.com>
2021-06-15 16:36:55 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,5 +28,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/aspect-ratio')],
+  plugins: [require('@tailwindcss/forms')],
 };
