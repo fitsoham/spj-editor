deea02cb28e831ffa823c5d945e92391fe433178
saurabh sachan <saurabh.sachan@spacejoy.com>
2021-06-22 23:45:24 +0530

    update

--- components/Home/Process.tsx
+++ components/Home/Process.tsx
@@ -15,72 +15,77 @@ const Process: React.FC = () => {
         description="Our design experts will transform any room in your home on our smart 3D desktop App. Shop handpicked products within your budget and style, directly from your room design, within Spacejoy."
       />
       <Controller>
-        <Scene duration={3500} triggerHook="onLeave" pin={true} indicators={false} offset={-80}>
-          <Timeline
-            labels={[
-              {
-                label: 'final',
-                position: 4,
-              },
-            ]}
-          >
-            <div className="relative container mx-auto rounded-2xl shadow-xl overflow-hidden">
-              <div className="next-image-fix mx-auto inset-0 bg-indigo-200 rounded-2xl">
-                <Image
-                  className="object-cover rounded-2xl"
-                  src="https://res.cloudinary.com/spacejoy/image/upload/v1624342360/spj-v2/transition/hp-transformation-empty_q8w3ma.jpg"
-                  height="900"
-                  width="1700"
-                />
-              </div>
-              <Tween from={{ opacity: 0, y: -20 }} to={{ opacity: 1, y: 0 }}>
-                <div className="absolute next-image-fix mx-auto inset-0 bg-pink-200 rounded-2xl">
-                  <Image
-                    className="object-cover rounded-2xl"
-                    src="https://res.cloudinary.com/spacejoy/image/upload/v1624342360/spj-v2/transition/hp-transformation-scandinavian_rilssr.jpg"
-                    height="900"
-                    width="1700"
-                  />
-                </div>
-              </Tween>
-              <Tween from={{ opacity: 0 }} to={{ opacity: 1 }}>
-                <div className="absolute next-image-fix mx-auto inset-0 bg-yellow-200 rounded-2xl">
-                  <Image
-                    className="object-cover rounded-2xl"
-                    src="https://res.cloudinary.com/spacejoy/image/upload/v1624342360/spj-v2/transition/hp-transformation-modern_wqhnru.jpg"
-                    height="900"
-                    width="1700"
-                  />
-                </div>
-              </Tween>
-              <Tween from={{ opacity: 0 }} to={{ opacity: 1 }}>
-                <div className="absolute next-image-fix mx-auto inset-0 bg-blue-200 rounded-2xl">
-                  <Image
-                    className="object-cover rounded-2xl"
-                    src="https://res.cloudinary.com/spacejoy/image/upload/v1624342360/spj-v2/transition/hp-transformation-industrial_npa9wm.jpg"
-                    height="900"
-                    width="1700"
-                  />
-                </div>
-              </Tween>
-              <Tween from={{ opacity: 0 }} to={{ opacity: 1 }}>
-                <div className="absolute mx-auto inset-0 bg-gray-900 rounded-2xl flex items-center justify-center">
-                  <div>
-                    <h2 className="text-9xl text-white text-center">Lets Start</h2>
-                    <div className="mt-10 text-center group">
-                      <Link href="/interior-designs">
-                        <a className="shadow-sm text-gray-900 py-3 px-6 rounded-xl bg-white inline-block">
-                          <ArrowRightIcon className="h-8 w-8 transition group-hover:translate-x-2" />
-                        </a>
-                      </Link>
+        <div className="relative container mx-auto px-4 rounded-2xl">
+          <Scene duration={3500} triggerHook="onLeave" pin={true} indicators={false} offset={-80}>
+            <Timeline
+              labels={[
+                {
+                  label: 'two',
+                  position: 1,
+                },
+                {
+                  label: 'final',
+                  position: 4,
+                },
+              ]}
+              target={
+                <>
+                  <div className="next-image-fix mx-auto inset-0 bg-indigo-200 rounded-2xl shadow-lg">
+                    <Image
+                      className="object-cover rounded-2xl"
+                      src="https://res.cloudinary.com/spacejoy/image/upload/v1624342360/spj-v2/transition/hp-transformation-empty_q8w3ma.jpg"
+                      height="900"
+                      width="1700"
+                    />
+                  </div>
+                  <div className="absolute next-image-fix mx-auto inset-0 bg-pink-200 rounded-2xl">
+                    <Image
+                      className="object-cover rounded-2xl"
+                      src="https://res.cloudinary.com/spacejoy/image/upload/v1624342360/spj-v2/transition/hp-transformation-scandinavian_rilssr.jpg"
+                      height="900"
+                      width="1700"
+                    />
+                  </div>
+                  <div className="absolute next-image-fix mx-auto inset-0 bg-yellow-200 rounded-2xl">
+                    <Image
+                      className="object-cover rounded-2xl"
+                      src="https://res.cloudinary.com/spacejoy/image/upload/v1624342360/spj-v2/transition/hp-transformation-modern_wqhnru.jpg"
+                      height="900"
+                      width="1700"
+                    />
+                  </div>
+                  <div className="absolute next-image-fix mx-auto inset-0 bg-blue-200 rounded-2xl">
+                    <Image
+                      className="object-cover rounded-2xl"
+                      src="https://res.cloudinary.com/spacejoy/image/upload/v1624342360/spj-v2/transition/hp-transformation-industrial_npa9wm.jpg"
+                      height="900"
+                      width="1700"
+                    />
+                  </div>
+                  <div className="absolute mx-auto inset-0 bg-gray-500 rounded-2xl flex items-center justify-center">
+                    <div>
+                      <h2 className="text-9xl text-white text-center">Lets Start</h2>
+                      <div className="mt-10 text-center group">
+                        <Link href="/interior-designs">
+                          <a className="shadow-sm text-gray-500 py-3 px-6 rounded-full bg-white inline-block">
+                            <ArrowRightIcon className="h-8 w-8 transition group-hover:translate-x-2" />
+                          </a>
+                        </Link>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </Tween>
+                </>
+              }
+            >
+              <Tween target={0} />
+              <Tween from={{ opacity: 0, y: -20 }} to={{ opacity: 1, y: 0 }} target={1} position="two" />
+              <Tween from={{ opacity: 0 }} to={{ opacity: 1 }} target={2} />
+              <Tween from={{ opacity: 0 }} to={{ opacity: 1 }} target={3} />
+              <Tween from={{ opacity: 0 }} to={{ opacity: 1 }} target={4} />
               <Tween position="final" />
-            </div>
-          </Timeline>
-        </Scene>
+            </Timeline>
+          </Scene>
+        </div>
       </Controller>
     </div>
   );
