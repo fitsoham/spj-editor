6b744314ebe594453fa63975d55101072cce5b44
saurabh sachan <saurabh.sachan@spacejoy.com>
2021-06-29 10:40:47 +0530

    updates

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,13 +32,8 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="icon" href="favicon.ico" />
           <link rel="manifest" href="/manifest.json" />
-          <link rel="prefetch" href="//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-          />
-          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet" />
+          <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+          <link href="//fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
