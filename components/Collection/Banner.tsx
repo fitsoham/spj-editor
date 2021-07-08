166f5f9b05448178ecf7a3f0a026767f1cea53a0
saurabh sachan <saurabh.sachan@spacejoy.com>
2021-07-08 09:34:02 +0530

    update

--- components/Collection/Banner.tsx
+++ components/Collection/Banner.tsx
@@ -1,5 +1,4 @@
 import { ArrowNarrowDownIcon } from '@heroicons/react/outline';
-import blurredBg from '@public/images/bg-base-64';
 import { cloudinary } from '@utils/config';
 import Image from 'next/image';
 import React from 'react';
@@ -33,21 +32,12 @@ const CollectionBanner: React.FC<BannerProps> = ({ data }) => {
   const date = new Date(data?.publishedDate || '');
   const formattedDate = `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
   return (
-    <div className="container relative mx-auto px-4">
-      <div className="relative bg-gray-200 rounded-2xl overflow-hidden">
+    <div className="container mx-auto px-4">
+      <div className="flex bg-gray-50 rounded-xl overflow-hidden items-center border border-gray-100 bg-gradient-to-r from-red-100 to-indigo-50">
         <Tween from={{ opacity: 0, x: -20 }} to={{ opacity: 1, x: 0 }} duration={1}>
-          <div className="next-image-fix absolute inset-0">
-            <Image
-              className="object-cover"
-              src={blurredBg}
-              alt="spacejoy happy customer"
-              height={'700'}
-              width={'1500'}
-            />
-          </div>
           <div className="next-image-fix">
             <Image
-              className="object-cover"
+              className="object-cover rounded-l-xl"
               src={`${cloudinary.baseDeliveryURL}/${data?.coverImg}`}
               alt="spacejoy happy customer"
               height={'700'}
@@ -55,10 +45,10 @@ const CollectionBanner: React.FC<BannerProps> = ({ data }) => {
             />
           </div>
         </Tween>
-        <div className="p-8 absolute bottom-0 left-0 bg-white m-8 rounded-xl">
-          <Tween from={{ opacity: 0, y: 50 }} to={{ opacity: 1, y: 0 }} duration={1} stagger={0.5}>
+        <div className="p-8">
+          <Tween from={{ opacity: 0, x: 50 }} to={{ opacity: 1, x: 0 }} duration={1} stagger={0.5}>
+            <p className="text-sm text-opacity-70 text-gray-600">{formattedDate}</p>
             <h1 className="sm:text-3xl md:text-5xl text-gray-600 mt-4 mb-4 max-w-xl leading-loose">{data?.name}</h1>
-            <p className="text-sm text-opacity-70 text-gray-600">Published Date: {formattedDate}</p>
             <p className="h-24 mt-4 overflow-hidden overflow-ellipsis text-justify text-sm max-w-xl">
               {data?.description}
             </p>
