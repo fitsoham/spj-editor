8fd5e6227b5549570652fbb6577659660bc354d1
saurabh sachan <saurabh.sachan@spacejoy.com>
2021-07-16 10:13:48 +0530

    improvements

--- components/Collection/Banner.tsx
+++ components/Collection/Banner.tsx
@@ -1,3 +1,4 @@
+import { ArrowNarrowRightIcon } from '@heroicons/react/outline';
 import { cloudinary } from '@utils/config';
 import Image from 'next/image';
 import React from 'react';
@@ -31,27 +32,31 @@ const CollectionBanner: React.FC<BannerProps> = ({ data }) => {
   const date = new Date(data?.publishedDate || '');
   const formattedDate = `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
   return (
-    <div className="container mx-auto px-4">
-      <div className="flex bg-gray-50 rounded-xl overflow-hidden items-center border border-gray-100 bg-gradient-to-r from-red-100 to-indigo-50">
-        <Tween from={{ opacity: 0, x: -20 }} to={{ opacity: 1, x: 0 }} duration={1}>
-          <div className="next-image-fix">
-            <Image
-              className="object-cover rounded-l-xl"
-              src={`${cloudinary.baseDeliveryURL}/${data?.coverImg}`}
-              alt="spacejoy happy customer"
-              height={'700'}
-              width={'1500'}
-            />
-          </div>
-        </Tween>
-        <div className="p-8">
+    <div className="container mx-auto px-4 py-10">
+      <div className="flex overflow-hidden items-center xl:space-x-10 2xl:space-x-20">
+        <div className="">
           <Tween from={{ opacity: 0, x: 50 }} to={{ opacity: 1, x: 0 }} duration={1} stagger={0.5}>
             <p className="text-sm text-opacity-70 text-gray-600">{formattedDate}</p>
-            <h1 className="sm:text-3xl md:text-5xl text-gray-600 mt-4 mb-4 max-w-xl leading-loose">{data?.name}</h1>
-            <p className="h-24 mt-4 overflow-hidden overflow-ellipsis text-justify text-sm max-w-xl">
+            <h1 className="sm:text-3xl md:text-5xl text-gray-900 mt-2 mb-4 max-w-xl leading-loose">{data?.name}</h1>
+            <p className="mt-4 mb-8 overflow-hidden overflow-ellipsis text-sm text-gray-600 max-w-xl">
               {data?.description}
             </p>
+            <button
+              type="button"
+              className="group shadow-xs hover:shadow-md text-base text-white py-3 px-12 rounded-xl bg-gray-900 focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-400 focus:outline-none"
+            >
+              Start Project <ArrowNarrowRightIcon className="inline h-4 w-4 transition group-hover:translate-x-1" />
+            </button>
           </Tween>
+        </div>
+        <div className="next-image-fix rounded-lg">
+          <Image
+            className="object-cover rounded-lg"
+            src={`${cloudinary.baseDeliveryURL}/v1626409657/spj-v2/bed_f8ioir.jpg`}
+            alt="spacejoy happy customer"
+            height={'404'}
+            width={'728'}
+          />
         </div>
       </div>
     </div>
