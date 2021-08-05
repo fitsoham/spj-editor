1a085556d305eae7f0a759cf6b43b6161b4a7e88
saurabh sachan <saurabh.sachan@spacejoy.com>
2021-08-05 10:10:43 +0530

    emaji added

--- components/Playground/MoreActions.tsx
+++ components/Playground/MoreActions.tsx
@@ -6,6 +6,7 @@ import {
   ShareIcon,
   SparklesIcon,
 } from '@heroicons/react/outline';
+import Image from 'next/image';
 import React from 'react';
 import UnitActionButton from './UnitActionButton';
 
@@ -30,6 +31,38 @@ const MoreActions: React.FC = () => {
         <UnitActionButton>
           <EmojiHappyIcon className="h-4 w-4" />
         </UnitActionButton>
+        <div className="next-image-fix">
+          <Image
+            src="https://res.cloudinary.com/spacejoy/image/upload/w_70/v1628138169/spj-v2/DIY/emojis/angry_i0zybg.png"
+            alt="angry"
+            height="35"
+            width="35"
+          />
+        </div>
+        <div className="next-image-fix">
+          <Image
+            src="https://res.cloudinary.com/spacejoy/image/upload/w_70/v1628138185/spj-v2/DIY/emojis/okay_yjadc4.png"
+            alt="angry"
+            height="35"
+            width="35"
+          />
+        </div>
+        <div className="next-image-fix">
+          <Image
+            src="https://res.cloudinary.com/spacejoy/image/upload/w_70/v1628138205/spj-v2/DIY/emojis/good_bqckuq.png"
+            alt="angry"
+            height="35"
+            width="35"
+          />
+        </div>
+        <div className="next-image-fix">
+          <Image
+            src="https://res.cloudinary.com/spacejoy/image/upload/w_70/v1628138169/spj-v2/DIY/emojis/awesome_hsitft.png"
+            alt="angry"
+            height="35"
+            width="35"
+          />
+        </div>
       </div>
       <div className="rounded-full bg-white p-2 flex-col space-y-2 shadow-sm">
         <UnitActionButton>
